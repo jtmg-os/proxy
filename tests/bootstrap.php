6e8404ea5ebaaed03fcdc637cdd1d584341e59bb
Jacek Trefon <jack@trefon.com>
2016-07-10 23:23:24 +0200

    cleanup in tests layout

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,1 @@
 <?php
-// Command that starts the built-in web server
-$command = 'php server.php';
-
-// Execute the command and store the process ID
-
-//shell_exec($command);
-
-//echo "Starting server" . PHP_EOL;
-
-// Kill the web server when the process ends
-register_shutdown_function(function() {
-//    exec('killall php ');
-//    echo "killed server" . PHP_EOL;
-});
